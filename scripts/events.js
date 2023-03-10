d8be66c37475121a5bec7a9ac076d0efdca01dd7
Ray Liu <aliciaraine1166@gmail.com>
2023-03-10 11:47:24 -0800

    add "add event" functionality

---
+++ scripts/events.js
@@ -0,0 +1,10 @@
+// $("#add-event-btn").click(function () {
+//   $("#add-event-form").show();
+// });
+
+// $(document).ready(setup);
+
+function show_form(x) {
+  if (x == 1) document.getElementById("#add-event-form").style.display = "none";
+  else document.getElementById("add-event-form").style.display = "block";
+}
