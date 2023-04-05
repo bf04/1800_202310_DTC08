a151b5c821f76c7f95d74b562ca13e51f008a041
Ray Liu <aliciaraine1166@gmail.com>
2023-04-05 04:53:53 -0700

    Remove firebase storage

--- scripts/firebaseapi.js
+++ scripts/firebaseapi.js
@@ -8,7 +8,7 @@ const firebaseConfig = {
   storageBucket: "team08project.appspot.com",
   messagingSenderId: "454792851968",
   appId: "1:454792851968:web:cf01c34f205dc04c729ee5",
-  measurementId: "G-W8TKKEZXT4"
+  measurementId: "G-W8TKKEZXT4",
 };
 
 //--------------------------------------------
@@ -17,4 +17,3 @@ const firebaseConfig = {
 //--------------------------------------------
 const app = firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
-const storage = firebase.storage();
